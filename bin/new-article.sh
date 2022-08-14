0ed8ed6003203b57527109bb5902def034ba21c1
gili-Katagiri <65004703+gili-Katagiri@users.noreply.github.com>
2022-08-14 20:22:05 +0900

    feat: add script for `zenn new:article` and set as yarn-script

---
+++ bin/new-article.sh
@@ -0,0 +1,26 @@
+#!/bin/env bash
+
+set -eu
+
+article_name=${1:-""}
+# Check if `article_name($1)` is exists
+if [ -z "$article_name" ]; then
+  echo "[Error]: Input new article name" 1>&2
+  exit 1
+fi
+
+function main() {
+    local _prefix=$(get_prefix)
+
+    # Ex) "2022-12-15_pman-nasub-moyac"
+    yarn zenn new:article --slug "${_prefix}_${article_name}"
+
+    return 0
+}
+
+function get_prefix() {
+    date +%Y-%m-%d
+    return 0
+}
+
+main $1
